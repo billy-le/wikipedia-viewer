551c269aeaff204682778db0a2f32be57a77922c
Billy Le <lebilly87@gmail.com>
2016-11-18 12:15:16 -0800

    updated search.js

--- src/search.js
+++ src/search.js
@@ -2,25 +2,25 @@ import ajax from './ajax.js';
 import results from './results.js';
 
 var search = function() {
-  //CACHE DOM
+  //cache DOM
   var el = document.getElementById('search-value');
   var btn = document.getElementById('search-button');
   var rdm = document.getElementById('random-button');
 
-  //BINDING EVENTS
+  //binding events
   btn.addEventListener('click', function(e){
     e.preventDefault();
     searchWiki();
   });
   rdm.addEventListener('click', randomWiki);
 
-  //EVENT HANDLERS
+  //event handlers
   function randomWiki() {
     window.open("https://en.wikipedia.org/wiki/Special:Random");
   };
   function searchWiki() {
     var input = document.getElementById('search-value');
-    ajax('GET', "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages||extracts&generator=search&piprop=thumbnail&pilimit=10&pithumbsize=200&exsentences=3&exlimit=10&exintro=1&explaintext=1&gsrlimit=10&gsrnamespace=0&gsrsearch=" + input.value, results);
+    ajax('GET', "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages||extracts||info&generator=search&piprop=thumbnail&pilimit=10&pithumbsize=200&exsentences=3&exlimit=10&exintro=1&explaintext=1&gsrlimit=10&gsrnamespace=0&inprop=url&gsrsearch=" + input.value, results);
     input.value = '';
   }
 };
